307c23339fcef89f79662eb464b5b79885632fc8
Adrien Constant <constant.adri@gmail.com>
2026-02-10 08:52:16 +0100

    Refactor: Remove unnecessary semicolons and clean up code formatting in Objet.py

--- Codes/1_Pb_sens_direct/Objet.py
+++ Codes/1_Pb_sens_direct/Objet.py
@@ -21,16 +21,16 @@ import matplotlib.pyplot as plt
 NbHO = 1280
 NbVO = 800
 #Rayon sphere mm
-R = 360;
+R = 360
 #Recul sphere mm
-a = 300;
+a = 300
 
 #Coordonnées matricielles des pts M de l'objet
-[X,Y] = meshgrid(linspace(-600,600,NbHO),linspace(-375,375,NbVO));
+[X,Y] = meshgrid(linspace(-600,600,NbHO),linspace(-375,375,NbVO))
 
 #Affixe de l'objet (mm)
-Za2 = R**2 - X**2 - Y**2;
-Z = sqrt((Za2>a**2)*Za2) - a + a*(Za2<=a**2);
+Za2 = R**2 - X**2 - Y**2
+Z = sqrt((Za2>a**2)*Za2) - a + a*(Za2<=a**2)
 #Enregistrement des coordonnées matricelles objet
 savetxt('X.txt', X, fmt='%-7.6f')   
 savetxt('Y.txt', Y, fmt='%-7.6f')
