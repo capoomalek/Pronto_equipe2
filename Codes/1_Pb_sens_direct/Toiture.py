8244346e3aff42f3059b458857e2e09b8fa26833
Adrien Constant <constant.adri@gmail.com>
2026-02-10 09:34:09 +0100

    Refactor: Update file names for saved matrices and adjust plot title in Toiture.py

--- Codes/1_Pb_sens_direct/Toiture.py
+++ Codes/1_Pb_sens_direct/Toiture.py
@@ -28,9 +28,9 @@ a = 300
 Za2 = R**2 - X**2 - Y**2
 Z = sqrt((Za2>a**2)*Za2) - a + a*(Za2<=a**2)
 #Enregistrement des coordonnées matricelles objet
-savetxt('X.txt', X, fmt='%-7.6f')   
-savetxt('Y.txt', Y, fmt='%-7.6f')
-savetxt('Z.txt', Z, fmt='%-7.6f')  
+savetxt('X_toiture.txt', X, fmt='%-7.6f')   
+savetxt('Y_toiture.txt', Y, fmt='%-7.6f')
+savetxt('Z_toiture.txt', Z, fmt='%-7.6f')  
 
 
 #************************************************************************
@@ -40,9 +40,9 @@ savetxt('Z.txt', Z, fmt='%-7.6f')
 z_min, z_max = 0, abs(Z).max()
 plt.figure()
 plt.pcolor(X,Y,Z, cmap='gray', vmin=z_min, vmax=z_max)
-plt.title('Z (mm) - Objet bouclier simulé')
+plt.title('Z (mm) - Objet toiture simulé')
 # set the limits of the plot to the limits of the data
 plt.axis([X.min(), X.max(), Y.min(), Y.max()])
 plt.colorbar()
-plt.savefig("Objet1.png")
+plt.savefig("Toiture1.png")
 print(time.process_time() - start_time, "seconds")  # fin mesure temps d'éxecusion
