358f9d3f5613cfe631527836086c8872b315892e
q25lepri <quentin.le-prince@imt-atlantique.net>
2026-02-24 10:24:51 +0100

    pupupuuuuush

---
+++ Codes/4_Nos_codes/Coord3D_objet.py
@@ -0,0 +1,14 @@
+import time
+start_time = time.process_time() # début mesure temps d'éxecusion
+
+# On importe le module numpy qui permet de faire du calcul numérique
+import numpy as np
+from numpy import loadtxt, linspace, sin, cos, pi, array, concatenate, savetxt, meshgrid, isnan
+from skimage import io
+from scipy.interpolate import griddata
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+
+
